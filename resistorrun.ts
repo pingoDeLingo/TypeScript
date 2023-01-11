1cfc702623e0ded9512b5a3406dc61c7a81fa148
Robin <robin.noormets@voco.ee>
2023-01-11 13:23:46 +0200

    Ülesanne TS-1 - voolude arvutamine

--- resistorrun.ts
+++ resistorrun.ts
@@ -7,6 +7,17 @@ class Resistor {
         return u / this.r;
     }
 }
-
 let r1: Resistor = new Resistor(220);
 console.log(r1.getCurrent(5));
+let r2: Resistor = new Resistor(110);
+console.log(r2.getCurrent(5));
+let r3: Resistor = new Resistor(4700);
+console.log(r3.getCurrent(5));
+
+let resistors: Resistor[] = [];
+resistors.push(new Resistor(220));
+resistors.push(new Resistor(4700));
+resistors.push(new Resistor(110));
+let totalCurrent: number = 0;
+resistors.forEach((resistor) => { totalCurrent += resistor.getCurrent(5); });
+console.log(totalCurrent);
